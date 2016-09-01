aac72e5796670fc158a1fbda075d2e613d40e614
Michel Machado <michel@digirati.com.br>
2016-09-01 13:36:31 -0400

    gatekeeper: add license note to all source files

--- include/gatekeeper_rt.h
+++ include/gatekeeper_rt.h
@@ -1,4 +1,21 @@
-/* XXX Which license should be placed here? */
+/*
+ * Gatekeeper - DoS protection system.
+ * Copyright (C) 2016 Digirati LTDA.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _GATEKEEPER_RT_H_
 #define _GATEKEEPER_RT_H_
 
