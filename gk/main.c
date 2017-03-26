25feb63568a1a91f193311b8ae170b062a115a91
mengxiang0811 <mengxiang0811@gmail.com>
2017-03-26 18:00:01 -0400

    gk: fix bugs in passing parameter for inet_ntop()

--- gk/main.c
+++ gk/main.c
@@ -535,28 +535,28 @@ print_flow_err_msg(struct ip_flow *flow, const char *err_msg)
 
 	if (flow->proto == ETHER_TYPE_IPv4) {
 		if (inet_ntop(AF_INET, &flow->f.v4.src,
-				src, sizeof(struct in_addr)) == NULL) {
+				src, sizeof(src)) == NULL) {
 			RTE_LOG(ERR, GATEKEEPER, "gk: %s: failed to convert a number to an IPv4 address (%s)\n",
 				__func__, strerror(errno));
 			return;
 		}
 
 		if (inet_ntop(AF_INET, &flow->f.v4.dst,
-				dst, sizeof(struct in_addr)) == NULL) {
+				dst, sizeof(dst)) == NULL) {
 			RTE_LOG(ERR, GATEKEEPER, "gk: %s: failed to convert a number to an IPv4 address (%s)\n",
 				__func__, strerror(errno));
 			return;
 		}
 	} else if (likely(flow->proto == ETHER_TYPE_IPv6)) {
 		if (inet_ntop(AF_INET6, flow->f.v6.src,
-				src, sizeof(struct in6_addr)) == NULL) {
+				src, sizeof(src)) == NULL) {
 			RTE_LOG(ERR, GATEKEEPER, "gk: %s: failed to convert a number to an IPv6 address (%s)\n",
 				__func__, strerror(errno));
 			return;
 		}
 
 		if (inet_ntop(AF_INET6, flow->f.v6.dst,
-				dst, sizeof(struct in6_addr)) == NULL) {
+				dst, sizeof(dst)) == NULL) {
 			RTE_LOG(ERR, GATEKEEPER, "gk: %s: failed to convert a number to an IPv6 address (%s)\n",
 				__func__, strerror(errno));
 			return;
