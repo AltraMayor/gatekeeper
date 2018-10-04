mengxiang0811/gatekeeper-1

gatekeeper: make GATEKEEPER_MAX_PKT_BURST parameter runtime parameter

include/gatekeeper_gk.h
@@ -45,6 +45,9 @@ enum gk_flow_state { GK_REQUEST, GK_GRANTED, GK_DECLINED };
 struct gk_instance {
 	struct rte_hash   *ip_flow_hash_table;
 	struct flow_entry *ip_flow_entry_table;
+
+	struct acl_search *acl;
+
 	/* RX queue on the front interface. */
 	uint16_t          rx_queue_front;
 	/* TX queue on the front interface. */

AltraMayor/gatekeeper

include/gatekeeper_gk.h
@@ -45,6 +45,10 @@ enum gk_flow_state { GK_REQUEST, GK_GRANTED, GK_DECLINED };
 struct gk_instance {
 	struct rte_hash   *ip_flow_hash_table;
 	struct flow_entry *ip_flow_entry_table;
+
+	struct acl_search *acl4;
+	struct acl_search *acl6;
+
 	/* RX queue on the front interface. */
 	uint16_t          rx_queue_front;
 	/* TX queue on the front interface. */