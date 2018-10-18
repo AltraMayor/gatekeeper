mengxiang0811/gatekeeper-1

sol: make all compile-time parameters runtime parameters

include/gatekeeper_sol.h
@@ -111,6 +111,9 @@ struct sol_config {
 
 	unsigned           mailbox_mem_cache_size;
 
+	/* Token bucket rate approximation error. */
+	double             tb_rate_config_err;
+
 	/*
 	 * The fields below are for internal use.
 	 * Configuration files should not refer to them.

lua/gatekeeper.lua
@@ -206,6 +206,7 @@ struct sol_config {
 	unsigned int enq_burst_size;
 	unsigned int deq_burst_size;
 	unsigned     mailbox_mem_cache_size;
+	double       tb_rate_config_err;
 	/* This struct has hidden fields. */
 };
 

sol/main.c
@@ -297,9 +297,6 @@ iface_speed_bytes(struct gatekeeper_if *iface, uint64_t *link_speed_bytes)
 	return 0;
 }
 
-/* Token bucket rate approximation error. */
-#define GATEKEEPER_TB_RATE_CONFIG_ERR (1e-7)
-
 /*
  * @sol_conf is allocated using rte_calloc(), so initializations
  * to 0 are not strictly necessary in this function.
@@ -347,7 +344,7 @@ req_queue_init(struct sol_config *sol_conf)
 	req_queue->cycles_per_byte_floor = cycles_per_byte_precise;
 	ret = rte_approx(
 		cycles_per_byte_precise - req_queue->cycles_per_byte_floor,
-		GATEKEEPER_TB_RATE_CONFIG_ERR, &a, &b);
+		sol_conf->tb_rate_config_err, &a, &b);
 	if (ret < 0) {
 		RTE_LOG(ERR, GATEKEEPER, "sol: could not approximate the request queue's allocated bandwidth\n");
 		return ret;

AltraMayor/gatekeeper

include/gatekeeper_sol.h
@@ -109,6 +109,9 @@ struct sol_config {
 	unsigned int       enq_burst_size;
 	unsigned int       deq_burst_size;
 
+	/* Token bucket rate approximation error. */
+	double             tb_rate_approx_err;
+
 	/*
 	 * The fields below are for internal use.
 	 * Configuration files should not refer to them.

lua/gatekeeper.lua
@@ -190,6 +190,7 @@ struct sol_config {
 	double       req_bw_rate;
 	unsigned int enq_burst_size;
 	unsigned int deq_burst_size;
+	double       tb_rate_approx_err;
 	/* This struct has hidden fields. */
 };
 

sol/main.c
@@ -297,9 +297,6 @@ iface_speed_bytes(struct gatekeeper_if *iface, uint64_t *link_speed_bytes)
 	return 0;
 }
 
-/* Token bucket rate approximation error. */
-#define GATEKEEPER_TB_RATE_CONFIG_ERR (1e-7)
-
 /*
  * @sol_conf is allocated using rte_calloc(), so initializations
  * to 0 are not strictly necessary in this function.
@@ -347,7 +344,7 @@ req_queue_init(struct sol_config *sol_conf)
 	req_queue->cycles_per_byte_floor = cycles_per_byte_precise;
 	ret = rte_approx(
 		cycles_per_byte_precise - req_queue->cycles_per_byte_floor,
-		GATEKEEPER_TB_RATE_CONFIG_ERR, &a, &b);
+		sol_conf->tb_rate_approx_err, &a, &b);
 	if (ret < 0) {
 		RTE_LOG(ERR, GATEKEEPER, "sol: could not approximate the request queue's allocated bandwidth\n");
 		return ret;