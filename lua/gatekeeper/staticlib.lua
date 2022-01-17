22e16e9931ca573d7002092d7d4efafafd2d04cc
Qiaobin Fu <mengxiang0811@gmail.com>
2022-01-17 03:57:22 -0500

    sol: have log accounted on a functional block basis

--- lua/gatekeeper/staticlib.lua
+++ lua/gatekeeper/staticlib.lua
@@ -402,7 +402,6 @@ struct sol_config {
 	double       tb_rate_approx_err;
 	double       req_channel_bw_mbps;
 	uint32_t     log_level;
-	int          log_type;
 	uint32_t     log_ratelimit_interval_ms;
 	uint32_t     log_ratelimit_burst;
 	/* This struct has hidden fields. */
